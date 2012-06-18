e0410950320840a7c1e34d07251c7088b289f7b7
unknown <adw21@adw21-PC.(none)>
2012-06-17 23:47:21 -0400

    Initial test push to git. Let's see what happens?
    Signed-off-by: unknown <adw21@adw21-PC.(none)>

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin 
+from blog.models import Post
+
+class postPhotoInline(admin.TabularInline):
+	model = postPhoto
+
+class postAdmin(VersionAdmin):
+	inlines = [postPhotoInline,]
+
+admin.site.register(Post)
---
+++ blog/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from taggit.managers import TaggableManager
+
+
+# Blog Post
+class Post(models.Model):
+	title = models.CharField(max_length = 150)
+	body = models.TextField()
+	created = models.DateTimeField()
+	tags = TaggableManager()
+	photos = models.ManyToManyField(PostPhoto)
+	
+	def __unicode__(self):
+		return self.title
+
+
+
+#Photo attached to blog post
+class PostPhoto(models.Model):
+	name = models.CharField(max_length=100)
+	image = models.ImageField(upload_to="postPhotos/%Y/%m/%d",height_field='height',width_field='width')
+	caption = models.CharField(max_length=200)
+	tags = TaggableManager()
+	last_modified = models.DateTimeField(editable=False)
+	created = models.DateTimeField(editable=False)
+	width = models.IntegerField(editable=False)
+	height = models.IntegerField(editable=False)
+	assocPost = models.ForeignKey(Post)
+	
+	def __unicode__(self):
+		return self.name
+
+
+
+
+
+ 
